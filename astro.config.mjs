6b364610ac7dedd0611ef529883cccc7d77b9449
InfiniteMediaStudio <infinitemedia92@gmail.com>
2025-03-08 11:15:35 +0400

    installed vercel, tailwind and alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [alpinejs()]
+});
