11788a8acceda83350a451110ef89eb43b680e10
InfiniteMediaStudio <infinitemedia92@gmail.com>
2025-06-06 18:23:41 +0400

    supabase admin config changed.

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseAdmin = createClient(
-  'https://sctrwvkpatgmkxqoabfd.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjdHJ3dmtwYXRnbWt4cW9hYmZkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MTQzODEzOSwiZXhwIjoyMDU3MDE0MTM5fQ.IAwO2KWPvH4rT4fK2VmrcUVtrnNOvRDYzNmpMxs44NU', {
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_SERVICE_KEY, {
   auth: {
     flowType: "pkce",
     autoRefreshToken: true,
